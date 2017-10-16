c17a194fa0e9794c569dbb655f7ffc0d32a0007b
Johnny Five <johnnyfive@johnny-five.local>
2017-10-16 15:10:40 -0500

    Angular: Registration, +SuccessMessage

--- angular/registration/src/app/app.component.ts
+++ angular/registration/src/app/app.component.ts
@@ -11,13 +11,15 @@ import { User } from './user';
 export class AppComponent {
   users = [];
   user = new User();
+  successMessage = "";
 
   onSubmit() {
-    console.log("FORM SUBMITTING", this.user, this.users)
+    console.log("FORM SUBMITTING", this.user, this.users);
+    this.successMessage = "Thank you for registering with us " + this.user.firstName + ". We just sent you a confirmation email to " + this.user.email + ", and will send all mail to " + this.user.streetAddress + ", " + this.user.city + " " + this.user.state + ".";
     this.users.push(this.user);
     this.user = new User();
   }
-
+  
 
 
 
